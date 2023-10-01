8d56f4ba41e67dd290a71558d3f264e600361de6
David Bernabé <david.bern.pal@gmail.com>
2023-10-01 13:25:05 +0200

    Refactor with class youtube

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
 import os
-import pytube
-from moviepy.editor import *
+from src.youtupe import Youtube
 
-# Configura tu clave de API de YouTube
-from googleapiclient.discovery import build
+# Clave de la API de YouTube
 api_key = os.getenv('TOKEN_YOUTUBE')
 
-# Función para descargar un video de YouTube en formato MP3
-def download_video_as_mp3(video_url, output_path):
-    yt = pytube.YouTube(video_url)
-    stream = yt.streams.filter(only_audio=True).first()
-    stream.download(output_path=output_path)
-
-# Función para obtener información de un video de YouTube
-def get_video_info(video_id):
-    youtube = build('youtube', 'v3', developerKey=api_key)
-    video_info = youtube.videos().list(part='snippet', id=video_id).execute()
-    return video_info
-
-# Ejemplo de uso
 if __name__ == "__main__":
-    video_url = 'https://www.youtube.com/watch?v=LoSm6VkplJc'
+    video_url = 'https://www.youtube.com/watch?v=wNMZRTROYuw'
     output_path = './downloads'
 
-    download_video_as_mp3(video_url, output_path)
-    print("Video descargado como MP3 con éxito!")
+    youtube = Youtube(api_key)
+    youtube.download_video_audio_as_mp3(video_url, output_path)
+    print(youtube.get_audio_info())
+    print("Audio del video descargado como MP3 con éxito!")
---
+++ src/youtupe.py
@@ -0,0 +1,25 @@
+import os
+import pytube
+from googleapiclient.discovery import build
+
+class Youtube:
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def download_video_audio_as_mp3(self, video_url, output_path):
+        yt = pytube.YouTube(video_url)
+        stream = yt.streams.filter(only_audio=True).first()
+        stream.download(output_path=output_path)
+
+        video_title = yt.title
+        mp4_path = os.path.join(output_path, f"{video_title}.mp4")
+        mp3_path = os.path.join(output_path, f"{video_title}.mp3")
+
+        os.rename(mp4_path, mp3_path)
+
+    
+    def get_audio_info(self):
+        youtube = build('youtube', 'v3', developerKey=self.api_key)
+        audio_info  = youtube.videos().list(part='snippet', id=self).execute()
+        return audio_info
+        
